b2058d505c81646072e9d5d37d3ddd5bf6edac83
SchalterDev <schalter.dev@gmail.com>
2018-04-10 17:29:00 +0200

    Signature for create user

---
+++ src/main/java/group/greenbyte/lunchplanner/user/UserJson.java
@@ -0,0 +1,9 @@
+package group.greenbyte.lunchplanner.user;
+
+public class UserJson {
+
+    public String userName;
+    public String password;
+    public String mail;
+
+}
--- src/main/java/group/greenbyte/lunchplanner/user/UserLogic.java
+++ src/main/java/group/greenbyte/lunchplanner/user/UserLogic.java
@@ -1,4 +1,27 @@
 package group.greenbyte.lunchplanner.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserLogic {
+
+    private UserDao userDao;
+
+    /**
+     * Create a user
+     *
+     * @param userName his username
+     * @param password the unhashed password
+     * @param mail mail-adress
+     */
+    void createUser(String userName, String password, String mail) {
+
+    }
+
+    @Autowired
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
 }
