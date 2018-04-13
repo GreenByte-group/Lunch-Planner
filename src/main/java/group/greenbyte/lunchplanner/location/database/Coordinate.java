2eb48393fc3e00112bbb03072f78075bb07a66b0
SchalterDev <schalter.dev@gmail.com>
2018-04-13 13:19:38 +0200

    Datenbank initialisiert

---
+++ src/main/java/group/greenbyte/lunchplanner/location/database/Coordinate.java
@@ -0,0 +1,30 @@
+package group.greenbyte.lunchplanner.location.database;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Coordinate {
+
+    @Column
+    private double xCoordinate;
+
+    @Column
+    private double yCoordinate;
+
+    public double getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setxCoordinate(double xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public double getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setyCoordinate(double yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+}
