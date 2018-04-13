26d39343d6c1f61f41c0784bef42254febf4a97a
SchalterDev <schalter.dev@gmail.com>
2018-04-13 13:02:36 +0200

    Automatisierte Tests verbessert

--- src/test/java/group/greenbyte/lunchplanner/event/EventControllerTest.java
+++ src/test/java/group/greenbyte/lunchplanner/event/EventControllerTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -26,6 +27,7 @@ import static group.greenbyte.lunchplanner.Utils.getJsonFromObject;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration (classes = AppConfig.class)
 @WebAppConfiguration
+@ActiveProfiles("application-test.properties")
 public class EventControllerTest {
 
     private MockMvc mockMvc;
