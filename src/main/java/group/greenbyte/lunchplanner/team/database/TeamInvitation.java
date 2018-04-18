76637db3ba5fff22d324f106c2bb6d095ee9a6f4
Unknown <sn-z777@outlook.com>
2018-04-18 15:47:52 +0200

    Es wird keine IllegalStateException mehr geworfen

--- src/main/java/group/greenbyte/lunchplanner/team/database/TeamInvitation.java
+++ src/main/java/group/greenbyte/lunchplanner/team/database/TeamInvitation.java
@@ -3,9 +3,10 @@ package group.greenbyte.lunchplanner.team.database;
 import group.greenbyte.lunchplanner.user.database.User;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
-public class TeamInvitation {
+public class TeamInvitation implements Serializable {
 
     @Id
     @ManyToOne(cascade = CascadeType.ALL)
