b97410143527481ef3cfadc1f0eb727215f7a9cb
SchalterDev <schalter.dev@gmail.com>
2018-05-05 13:06:37 +0200

    Send invitations for event

--- frontend/lunchplanner/src/components/CreateEventFunctions.js
+++ frontend/lunchplanner/src/components/CreateEventFunctions.js
@@ -5,7 +5,27 @@ export function createEvent(locationId, date, member, visible, responseFunc, err
     let timeEnd = date.getTime();
     timeEnd = timeEnd + 1;
     let url =  HOST + '/event';
-        axios.post(url, {name: "dummy", description: "", locationId : locationId, timeStart: date, timeEnd: timeEnd})
-            .then(responseFunc)
-            .catch(errorFunc);
+    axios.post(url, {name: "dummy", description: "", locationId : locationId, timeStart: date, timeEnd: timeEnd})
+        .then((response) => {
+            if(response.status === 201) {
+                inviteMember(response.data, member);
+            }
+            responseFunc(response);
+        })
+        .catch(errorFunc);
+}
+
+// send invitations
+function inviteMember(eventId, member) {
+    let string = String(member);
+    if(string !== "") {
+        (string.split(',')).forEach((oneMember) => {
+            let url = HOST + "/event/" + oneMember + "/invite/event/" + eventId;
+            console.log(url);
+            axios.post(url)
+                .then((response) => {
+                    console.log(response);
+                })
+        });
+    }
 }
