2891d028285a4141dd49f221940cd260d84f6a2f
SchalterDev <schalter.dev@gmail.com>
2018-05-17 14:07:49 +0200

    Suche zum Dialog hinzugefügt

--- frontend/lunchplanner/src/components/Dialog.js
+++ frontend/lunchplanner/src/components/Dialog.js
@@ -6,9 +6,11 @@ import Toolbar from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
 import Typography from 'material-ui/Typography';
 import CloseIcon from '@material-ui/icons/Close';
+import SearchIcon from '@material-ui/icons/Search';
 import Slide from 'material-ui/transitions/Slide';
 import {Link} from "react-router-dom";
 import {getHistory} from "../utils/HistoryUtils";
+import {TextField} from "material-ui";
 
 function Transition(props) {
     return <Slide direction="up" {...props} />;
@@ -31,20 +33,47 @@ class Dialog extends React.Component {
         this.state = {
             title: props.title || "",
             onClose: props.onClose,
+            closeUrl: props.closeUrl,
             open: true,
+            onSearch: props.onSearch,
+            search: "",
+        }
+    }
+
+    componentWillReceiveProps(newProps) {
+        if(newProps.title && newProps.title !== this.props.title) {
+            this.setState({
+                title: newProps.title,
+            })
         }
     }
 
     onClose = () => {
         if(this.state.onClose) {
+            console.log("close function");
             this.state.onClose();
+        } else if(this.state.closeUrl) {
+            console.log("closeurl");
+           getHistory().push(this.state.closeUrl);
         } else {
+            console.log("close go back");
             getHistory().goBack();
         }
     };
 
+    searchChanged = (event) => {
+        this.setState({
+            search: event.target.value,
+        });
+
+        this.state.onSearch(event.target.value);
+    };
+
     render() {
         const { classes } = this.props;
+        const search = !!this.state.onSearch;
+
+        console.log("Search: " + search);
 
         return (
             <DialogMaterial
@@ -60,6 +89,18 @@ class Dialog extends React.Component {
                         <Typography variant="title" color="inherit" className={classes.flex}>
                             {this.state.title}
                         </Typography>
+
+                        {(search) ?
+                                <div>
+                                    <IconButton color="primary">
+                                        <SearchIcon />
+                                    </IconButton>
+                                    <TextField
+                                        value={this.state.search}
+                                        onChange={this.searchChanged}
+                                    />
+                                </div>
+                            : ""}
 
                     </Toolbar>
                 </AppBar>
