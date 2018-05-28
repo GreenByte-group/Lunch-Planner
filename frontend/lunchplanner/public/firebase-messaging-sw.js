7364962520cbf5f6702b53df2bfef5fe3812b837
SchalterDev <schalter.dev@gmail.com>
2018-05-28 10:20:38 +0200

    Click Action hinzugefügt

--- frontend/lunchplanner/public/firebase-messaging-sw.js
+++ frontend/lunchplanner/public/firebase-messaging-sw.js
@@ -16,13 +16,6 @@ const messaging = firebase.messaging();
 
 messaging.setBackgroundMessageHandler((payload) => {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    //TODO background notifications
-    var notificationTitle = 'Background Message Title';
-    var notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-    };
 
-    return self.registration.showNotification(notificationTitle,
-        notificationOptions);
+    return self.registration.showNotification();
 });
