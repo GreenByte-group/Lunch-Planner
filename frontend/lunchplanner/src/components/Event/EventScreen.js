af11a1fad6aa65e9dcb7c8555081e884f25492c1
chefkoch24 <kuennecke-felix@web.de>
2018-06-01 16:41:00 +0200

    Frontend invite extern (#97)
    * aktueller dev stand (#96)
    
    * Jest without eject (#94)
    
    * jest und enzyme laufen
    
    * Erster snapshot fertig
    
    * Tests verbessert
    
    * Fehler beim mergen behoben
    
    * Noch einen Fehler
    
    * Get Email
    
    * link für externe sichtbar
    
    * share Button wird angezeigt wenn es einmal geteilt wurde

--- frontend/lunchplanner/src/components/Event/EventScreen.js
+++ frontend/lunchplanner/src/components/Event/EventScreen.js
@@ -25,7 +25,6 @@ import {
     replyToEvent
 } from "./EventFunctions";
 import ShareIcon from "@material-ui/icons/Share"
-import InviteExtern from "../User/InviteExtern";
 
 function Transition(props) {
     return <Slide direction="up" {...props} />;
@@ -234,14 +233,16 @@ class EventScreen extends React.Component {
             people:[],
             accepted: false,
             isShared : false,
+            token: null,
         };
     }
 
     componentDidMount() {
-        let eventName, description, date, people, accepted, location, eventId;
-        let token = this.props.match.params.securityToken;
+        let eventName, description, date, people, accepted, location, eventId, token;
+
+        token = this.props.match.params.securityToken;
+
         getEventExtern(token, (response) => {
-            console.log(response);
             this.setState({
                 eventId: response.data.eventId,
                 name: response.data.eventName,
@@ -249,6 +250,8 @@ class EventScreen extends React.Component {
                 date: new Date(response.data.startDate),
                 description: response.data.eventDescription,
                 people: response.data.invitations,
+                token:response.data.shareToken,
+                isShared: true,
             });
         });
 
@@ -272,6 +275,9 @@ class EventScreen extends React.Component {
             if (this.props.location.query.location) {
                 location = String(this.props.location.query.location);
             }
+            if (this.props.location.query.token) {
+                token = String(this.props.location.query.token);
+            }
 
             this.setState({
                 eventId: eventId,
@@ -281,7 +287,13 @@ class EventScreen extends React.Component {
                 people: people,
                 accepted: accepted,
                 location: location,
-            })
+                token: token,
+            });
+            if(this.state.token !== null){
+                this.setState({
+                    isShared: true,
+                });
+            }
         } else {
             this.loadEvent(eventId);
         }
@@ -371,8 +383,14 @@ class EventScreen extends React.Component {
                 location: response.data.location,
                 people: response.data.invitations,
                 date: new Date(response.data.startDate),
+                token: response.data.shareToken,
             })
         })
+        if(this.state.token !== null){
+            this.setState({
+                isShared: true,
+            });
+        }
     };
 
     handleDecline = () => {
@@ -408,11 +426,6 @@ class EventScreen extends React.Component {
         });
     };
 
-    handleShare = () =>{
-        this.setState({
-            isShared: true,
-        });
-    };
 
     render() {
         const { classes } = this.props;
@@ -423,6 +436,7 @@ class EventScreen extends React.Component {
         let location = this.state.location;
         let people = this.state.people;
         let eventId = this.state.eventId;
+        let isShared = this.state.isShared;
 
         if(people.length !== 0) {
             this.parseUrl();
@@ -451,7 +465,6 @@ class EventScreen extends React.Component {
         let accepted = false;
         let buttonText = "Join Event";
         let barTitle = name;
-        let isShared = this.state.isShared;
 
         let iAmAdmin = false;
 
@@ -540,7 +553,7 @@ class EventScreen extends React.Component {
                                 <Link to={{pathname:`/event/${eventId}/share`, query: {
                                         source: "/event/" + this.state.eventId}}}>
                                     <div className={classes.headerShare}>
-                                        <ShareIcon className={classes.shareIcon} onClick={this.handleShare}/>
+                                        <ShareIcon className={classes.shareIcon}/>
                                         <p className={classes.shareText}>Share</p>
                                     </div>
                                 </Link>
