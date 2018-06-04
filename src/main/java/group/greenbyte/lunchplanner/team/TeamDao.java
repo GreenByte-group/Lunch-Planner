f4e3c29d7d0bfcd54510074640819bd7d538c035
Zentino <sn-z777@outlook.com>
2018-06-04 20:02:17 +0200

    teamDao

--- src/main/java/group/greenbyte/lunchplanner/team/TeamDao.java
+++ src/main/java/group/greenbyte/lunchplanner/team/TeamDao.java
@@ -151,4 +151,13 @@ public interface TeamDao {
      * @param description new description of the team
      */
     void updateDescription(int teamId, String description) throws DatabaseException;
+
+    /**
+     * Remove a team member from database
+     *
+     * @param userToRemove user that is going to be deleted from database
+     * @param teamId id of the team
+     * @throws DatabaseException
+     */
+    void removeTeamMember(String userToRemove, int teamId) throws DatabaseException;
 }
