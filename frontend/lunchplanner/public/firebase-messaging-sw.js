d125cfc7fbcfadf19ba66aedcbfc1e4c2425a938
SchalterDev <schalter.dev@gmail.com>
2018-06-05 11:44:45 +0200

    worker überarbeitet

--- frontend/lunchplanner/public/firebase-messaging-sw.js
+++ frontend/lunchplanner/public/firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-import {FRONTEND_HOST} from "../src/Config";
+const FRONTEND_HOST = "https://lunchplanner.greenbyte.group";
 
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
