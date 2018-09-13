3850fc24c1f20694f91d778d7d0b30a426c9469a
Can <cangospcstuff@outlook.com>
2018-09-13 17:29:03 +0200

    changing frontendhost

--- frontend/lunchplanner/src/Config.js
+++ frontend/lunchplanner/src/Config.js
@@ -1,7 +1,7 @@
 export const HOST = "http://localhost:8090";
 export const TOKEN = "token_lunch_planner";
 export const USERNAME = "user_name_lunch_planner";
-export const FRONTEND_HOST = "http://localhost:3000";
+export const FRONTEND_HOST = "http://52.58.35.169:3000";
 
 export const configFirebase = {
     apiKey: "AIzaSyDyuySWwkXgZDrLnO0gX9bmGpR7XAHnngE",
